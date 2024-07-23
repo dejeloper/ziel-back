3c7d16ed13f7e8344d85dcecb5b37ed5a6a583d5
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-07-22 19:09:57 -0500

    feat: Add global validation pipe and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { configSwagger, pathSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(pathSwagger);
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup(pathSwagger, app, document);
+
   await app.listen(3000);
 }
 bootstrap();
